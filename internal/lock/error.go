080fdd4d69db898ec739f59ad87cb8d0e9981084
Aditya <aditya.kw90@gmail.com>
2026-01-04 10:00:45 +0700

    Fix formatting in error messages for lock handling
    - Adjusted spacing in the error message comment for ErrLockReleaseForbidden for improved readability and consistency.

--- internal/lock/error.go
+++ internal/lock/error.go
@@ -6,5 +6,5 @@ import "errors"
 var (
 	ErrLockAcquireFailed    = errors.New("failed to acquire lock")                       // lock acquisition failed.
 	ErrLockReleaseUnlocked  = errors.New("cannot release an unlocked lock")              // release a lock that doesn't exist.
-	ErrLockReleaseForbidden = errors.New("cannot release a lock that's no longer owned") //release a lock owned by someone else.
+	ErrLockReleaseForbidden = errors.New("cannot release a lock that's no longer owned") // release a lock owned by someone else.
 )
